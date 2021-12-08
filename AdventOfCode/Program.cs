18aea3f2800e2f728930070cab93fc01f2312918
chuck <chuck.bates@emmersion.ai>
2021-12-08 13:03:39 -0700

    reorder program to make new days easier

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,33 +2,15 @@
 using System.IO;
 using AdventOfCode;
 
-// var partOne = DayOne.PartOne(File.ReadAllLines("../../../inputs/dayOne.txt"));
-// var partTwo = DayOne.PartTwo(File.ReadAllLines("../../../inputs/dayOne.txt"));
-// Console.WriteLine("Day one results:");
-// Console.WriteLine($"Part one: {partOne}");
-// Console.WriteLine($"Part two: {partTwo}");
+var partOne = DayEight.PartOne(File.ReadAllLines("../../../inputs/dayEight.txt"));
+var partTwo = DayEight.PartTwo(File.ReadAllLines("../../../inputs/dayEight.txt"));
+Console.WriteLine("Day eight results:");
+Console.WriteLine($"Part one: {partOne}");
+Console.WriteLine($"Part two: {partTwo}");
 
-// var partOne = DayTwo.PartOne(File.ReadAllLines("../../../inputs/dayTwo.txt"));
-// var partTwo = DayTwo.PartTwo(File.ReadAllLines("../../../inputs/dayTwo.txt"));
-// Console.WriteLine("Day two results:");
-// Console.WriteLine($"Part one: {partOne}");
-// Console.WriteLine($"Part two: {partTwo}");
-
-// var partOne = DayThree.PartOne(File.ReadAllLines("../../../inputs/dayThree.txt"));
-// var partTwo = DayThree.PartTwo(File.ReadAllLines("../../../inputs/dayThree.txt"));
-// Console.WriteLine("Day three results:");
-// Console.WriteLine($"Part one: {partOne}");
-// Console.WriteLine($"Part two: {partTwo}");
-
-// var partOne = DayFour.PartOne(File.ReadAllLines("../../../inputs/dayFour.txt"));
-// var partTwo = DayFour.PartTwo(File.ReadAllLines("../../../inputs/dayFour.txt"));
-// Console.WriteLine("Day four results:");
-// Console.WriteLine($"Part one: {partOne}");
-// Console.WriteLine($"Part two: {partTwo}");
-
-// var partOne = DayFive.PartOne(File.ReadAllLines("../../../inputs/dayFive.txt"));
-// var partTwo = DayFive.PartTwo(File.ReadAllLines("../../../inputs/dayFive.txt"));
-// Console.WriteLine("Day five results:");
+// var partOne = DaySeven.PartOne(File.ReadAllLines("../../../inputs/daySeven.txt"));
+// var partTwo = DaySeven.PartTwo(File.ReadAllLines("../../../inputs/daySeven.txt"));
+// Console.WriteLine("Day seven results:");
 // Console.WriteLine($"Part one: {partOne}");
 // Console.WriteLine($"Part two: {partTwo}");
 
@@ -38,14 +20,32 @@ using AdventOfCode;
 // Console.WriteLine($"Part one: {partOne}");
 // Console.WriteLine($"Part two: {partTwo}");
 
-// var partOne = DaySeven.PartOne(File.ReadAllLines("../../../inputs/daySeven.txt"));
-// var partTwo = DaySeven.PartTwo(File.ReadAllLines("../../../inputs/daySeven.txt"));
-// Console.WriteLine("Day seven results:");
+// var partOne = DayFive.PartOne(File.ReadAllLines("../../../inputs/dayFive.txt"));
+// var partTwo = DayFive.PartTwo(File.ReadAllLines("../../../inputs/dayFive.txt"));
+// Console.WriteLine("Day five results:");
 // Console.WriteLine($"Part one: {partOne}");
 // Console.WriteLine($"Part two: {partTwo}");
 
-var partOne = DayEight.PartOne(File.ReadAllLines("../../../inputs/dayEight.txt"));
-var partTwo = DayEight.PartTwo(File.ReadAllLines("../../../inputs/dayEight.txt"));
-Console.WriteLine("Day eight results:");
-Console.WriteLine($"Part one: {partOne}");
-Console.WriteLine($"Part two: {partTwo}");
+// var partOne = DayFour.PartOne(File.ReadAllLines("../../../inputs/dayFour.txt"));
+// var partTwo = DayFour.PartTwo(File.ReadAllLines("../../../inputs/dayFour.txt"));
+// Console.WriteLine("Day four results:");
+// Console.WriteLine($"Part one: {partOne}");
+// Console.WriteLine($"Part two: {partTwo}");
+
+// var partOne = DayThree.PartOne(File.ReadAllLines("../../../inputs/dayThree.txt"));
+// var partTwo = DayThree.PartTwo(File.ReadAllLines("../../../inputs/dayThree.txt"));
+// Console.WriteLine("Day three results:");
+// Console.WriteLine($"Part one: {partOne}");
+// Console.WriteLine($"Part two: {partTwo}");
+
+// var partOne = DayTwo.PartOne(File.ReadAllLines("../../../inputs/dayTwo.txt"));
+// var partTwo = DayTwo.PartTwo(File.ReadAllLines("../../../inputs/dayTwo.txt"));
+// Console.WriteLine("Day two results:");
+// Console.WriteLine($"Part one: {partOne}");
+// Console.WriteLine($"Part two: {partTwo}");
+
+// var partOne = DayOne.PartOne(File.ReadAllLines("../../../inputs/dayOne.txt"));
+// var partTwo = DayOne.PartTwo(File.ReadAllLines("../../../inputs/dayOne.txt"));
+// Console.WriteLine("Day one results:");
+// Console.WriteLine($"Part one: {partOne}");
+// Console.WriteLine($"Part two: {partTwo}");
